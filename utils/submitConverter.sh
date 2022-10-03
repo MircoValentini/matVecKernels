ecf9ba0b79ad0a6a6c47e2122be07f5f69d6971d
Simone Bna <s.bn@cineca.it>
2022-10-03 12:32:55 +0200

    Improved readability of utils/submitConverter.sh

--- utils/submitConverter.sh
+++ utils/submitConverter.sh
@@ -3,14 +3,28 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=0
 #SBATCH --cpus-per-task=1
+#SBATCH --output=R-%x.%j.out
+#SBATCH --error=R-%x.%j.err
 #SBATCH --account=cin_staff
 #SBATCH --partition=m100_usr_prod
-#SBATCH --time 01:00:00
+#SBATCH --time 02:00:00
 
+set -e
 
-#
-# Load the environment needed to run the job
-cd /m100_work/ExaF_prod22/unitTests
+# User defined variables
+TEST_MATRICES_DIR=/m100_work/ExaF_prod22/wp2-matrices/TestMatrices/
+NUM_MAT=1
+NUM_VEC=1
+
+# Internal variables 
+if [ -n $SLURM_JOB_ID ] ; then
+  SCRIPT_PATH=$(scontrol show job $SLURM_JOBID | awk -F= '/Command=/{print $2}')
+else
+  SCRIPT_PATH="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
+fi
+ROOT_PATH=$(dirname $(dirname ${SCRIPT_PATH}))
+DATA_DIR=${ROOT_PATH}"/data"
+BIN_DIR=${ROOT_PATH}"/preProcessor/bin"
 
 #
 # Clean environment
@@ -25,18 +39,15 @@ module load zlib/1.2.11--gnu--8.4.0
 module load szip/2.1.1--gnu--8.4.0
 module load hdf5/1.12.0--gnu--8.4.0
 
-
 #
-# Clean all the repository
-for i in `find /m100_work/ExaF_prod22/wp2-matrices/TestMatrices/ -type f -iname 'owner'` ; do
-  prefix=`echo ${i} | sed -e '{s/^\///};{s/\/owner//};{s/\//_/g}' | sed -e 's/^m100_work_ExaF_prod22_wp2-matrices_TestMatrices_//'`;
-  ownerName=${i};
-  neighName=`echo ${i} | sed -e 's/owner/neighbour/'`;
-  outDir="./"
-  ./converter.x -o ${ownerName} -n ${neighName} -p ${prefix} -d ${outDir} -r 2 -t 5  >> convert000.log
+for filename in `find $TEST_MATRICES_DIR -type f -iname 'owner'` ; do
+  relName=$(realpath --relative-to=${TEST_MATRICES_DIR} ${filename}) 
+  relPath=$(dirname $(dirname ${relName}))
+  prefix=`echo ${relName} | sed -e '{s/^\///};{s/\/owner//};{s/\//_/g}'`
+  ownerName=${filename}
+  neighName=`echo ${filename} | sed -e 's/owner/neighbour/'`;
+  outDir=${DATA_DIR}"/"${relPath}
+  mkdir -p ${outDir}
+  ${BIN_DIR}"/FOAM2H5MatConverter" -o ${ownerName} -n ${neighName} -p ${prefix} -d ${outDir} -r ${NUM_MAT} -t ${NUM_VEC}
 done
 
-
-#
-# Exit with no error
-exit 0;
